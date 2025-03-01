060c2dbec74f215f4eaa38e282b869fdf7c90f01
Dev0907 <devparikh200479@gmail.com>
2025-03-01 11:57:08 +0530

    w

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+import userRouter from "./routes/user.routes.js"
+import commonPostRouter from "./routes/posts.routes.js"
+import likeRouter from "./routes/like.routes.js"
+
+app.use("/api/v1",userRouter)
+app.use("/api/v1/commonPosts",commonPostRouter)
+app.use("/api/v1/l",likeRouter)
+
+export {app}

3dbadefa066979858c2e7da2b1d8791cd6eb5a4a
Dev0907 <devparikh200479@gmail.com>
2025-03-01 11:39:09 +0530

    w

---
+++ backend/src/index.js
@@ -0,0 +1,35 @@
+import { connectDb } from "./db/index.js";
+import { app } from "./app.js";
+
+
+app.get('/',(req,res)=>{
+    res.send("Heyy")
+})
+app.get('/testing',(req,res)=>{
+    res.send("Heyy")
+})
+app.get('/api',(req,res)=>{
+    res.send("Heyy")
+})
+
+
+connectDb()
+.then(()=>{
+    app.listen(process.env.PORT||8003,()=>{
+        console.log("server on 8002");
+    })
+})
+.catch((err)=>{
+    console.log("Db err ",err);
+})
+
+
+console.log("Hello");
+
+connectDb()
+.then(()=>{
+    console.log("Connected to Postgres database");
+})
+.catch((error) => {
+    console.error("Error connecting to Postgres", error);
+});
---
+++ backend/src/models/post.model.js
@@ -0,0 +1,57 @@
+import {DataTypes} from "sequelize"
+import { connectDb } from "../db/index.js";
+
+const sequelize = await connectDb()
+
+const Post = sequelize.define("Post", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    image: {
+        type: DataTypes.STRING, // Optional image for posts
+        allowNull: true
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: "Users",
+            key: "id"
+        },
+        onDelete: "CASCADE"
+    },
+    community_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: "Communities",
+            key: "id"
+        },
+        allowNull: true, // If null, it's a personal post (like Twitter)
+        onDelete: "CASCADE"
+    },
+    likes_count: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+        type: DataTypes.DATE
+    }
+}, {
+    timestamps: false,
+    tableName: 'posts'
+});
+
+
+
+
+
+export { Post }
